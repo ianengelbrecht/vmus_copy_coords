bc0be508eb0fb6f5a0ea0b9f0c165079488ac4e8
Ian Engelbrecht <ianicus.za@gmail.com>
2023-04-07 20:54:12 +0200

    backup

--- popup.js
+++ popup.js
@@ -18,60 +18,70 @@ async function getCoords() {
 
   if(window.location.hostname == 'vmus.adu.org.za') {
     const tables = document.querySelectorAll('table')
-    if(tables && tables.length >= 6){
-      const dataTable = tables[2]
-      const rows = dataTable.tBodies[0].rows
-      const latRow = Array.from(rows).filter(x => x.innerText.startsWith('Decimal latitude'))
-      const longRow = Array.from(rows).filter(x => x.innerText.startsWith('Decimal longitude'))
+    if(tables && tables.length){
 
-      if(!latRow || !latRow.length || !longRow || !longRow.length){
-        alert('specimen coordinates not available on this page')
+      //we have to iterative because the number of tables can vary depending on the contents of the page 
+      //and there are no ids!
+
+      for(let i = 0; i < tables.length; i++) {
+        const dataTable = tables[i]
+        const rows = dataTable.tBodies[0].rows
+        const latRow = Array.from(rows).filter(x => x.innerText.startsWith('Decimal latitude'))
+        const longRow = Array.from(rows).filter(x => x.innerText.startsWith('Decimal longitude'))
+
+        if(!latRow || !latRow.length || !longRow || !longRow.length){
+          continue
+        }
+        //else
+
+        //add the toast, thank you w3 schools...
+        let toast = document.getElementById("snackbar");
+        if(!toast) {
+          toast = document.createElement('div')
+          toast.setAttribute('id', 'snackbar') //snackbar/toast, same thing
+          toast.innerText = "Coordinates copied!!"
+          document.body.appendChild(toast)
+        }
+        
+
+        let lat = latRow[0].innerText.trim().split(/\s+/)[2]
+        let long = longRow[0].innerText.trim().split(/\s+/)[2]
+
+        //fix the decimals...
+        let latParts = lat.split('.')
+        latParts[1] = latParts[1].substr(0, 5)
+        lat = latParts.join('.')
+
+        let longParts = long.split('.')
+        longParts[1] = longParts[1].substr(0, 5)
+        long = longParts.join('.')
+
+        let coords = `${lat}, ${long}`
+
+        //we can't use navigator.clipboard, so...
+        let copyFrom = document.createElement("textarea");
+        //copyFrom.hidden = true
+        copyFrom.textContent = coords;
+        document.body.appendChild(copyFrom);
+        copyFrom.select();
+        document.execCommand('copy'); //yes, deprecated, but it still works...
+        copyFrom.blur();
+        document.body.removeChild(copyFrom);
+
+        toast.className = 'show'
+        console.log('coords written to clipboard')
+        setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 1000);
         return
       }
 
-      //add the toast, thank you w3 schools...
-      let toast = document.getElementById("snackbar");
-      if(!toast) {
-        toast = document.createElement('div')
-        toast.setAttribute('id', 'snackbar') //snackbar/toast, same thing
-        toast.innerText = "Coordinates copied!!"
-        document.body.appendChild(toast)
-      }
+      //if we get here then we don't have coordinates
+      alert('specimen coordinates not available on this page')
+      return
       
-
-      let lat = latRow[0].innerText.trim().split(/\s+/)[2]
-      let long = longRow[0].innerText.trim().split(/\s+/)[2]
-
-      //fix the decimals...
-      let latParts = lat.split('.')
-      latParts[1] = latParts[1].substr(0, 5)
-      lat = latParts.join('.')
-
-      let longParts = long.split('.')
-      longParts[1] = longParts[1].substr(0, 5)
-      long = longParts.join('.')
-
-      let coords = `${lat}, ${long}`
-
-      //we can't use navigator.clipboard, so...
-      let copyFrom = document.createElement("textarea");
-      //copyFrom.hidden = true
-      copyFrom.textContent = coords;
-      document.body.appendChild(copyFrom);
-      copyFrom.select();
-      document.execCommand('copy'); //yes, deprecated, but it still works...
-      copyFrom.blur();
-      document.body.removeChild(copyFrom);
-
-      toast.className = 'show'
-      setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 1000);
-
-      console.log('coords written to clipboard')
     }
     else {
-      console.log(tables)
-      alert('this does not appear to be record page...')
+      alert('specimen coordinates not available on this page')
+      return
     }
-    
   }
 }
